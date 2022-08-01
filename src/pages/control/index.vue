d6981e5c75cecce507abe0045f667296a21c3d20
yunfeizhang@chutianyun.gov.cn <zyfjz9593>
2022-08-01 15:30:35 +0800

    添加登录以及管理页面

--- src/pages/control/index.vue
+++ src/pages/control/index.vue
@@ -1,41 +1,89 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
+  <view class="control">
+	<!-- 操作中心 -->
+    <div class="control_action">
+      <div class="control_action_item" v-for="(item,index) in controlList" :key="index" @click="handelClick(item)">
+        <image class="control_action_img" :src="item.imgUrl" mode=""></image>
+        <div class="control_action_title">{{item.name}}</div>
+      </div>
+    </div>
+	<!-- 事件信息 -->
+	<view class="control_event">
+	  <view class="control_event_name">
+	    <ModuleName :name="'事件中心'" />
+	  </view>
+	
+	</view>
   </view>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue'
-const title = ref('control')
+<script  lang="ts">
+import { defineComponent, ref } from 'vue'
+import ModuleName from '@/components/ModuleName/index.vue'
+
+export default defineComponent({
+  components: {
+    ModuleName
+  },
+  setup() {
+    const controlList = ref([
+      {
+        name:"视频中心",
+        imgUrl:"/static/control/video.png",
+        id: 0
+      },
+      {
+        name:"种植中心",
+        imgUrl:"/static/control/plant.png",
+        id: 1
+      },
+      {
+        name:"作业管理",
+        imgUrl:"/static/control/task.png",
+        id: 2
+      },
+      {
+        name:"事件中心",
+        imgUrl:"/static/control/event.png",
+        id: 3
+      },
+    ])
+    const handelClick =  ( item: any ) =>{
+      console.log(item)
+    }
+    return {
+      controlList,
+      handelClick
+    }
+   }
+})
 </script>
 
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
+<style lang="scss" scoped>
+.control{
+	background: #fff;
+	.control_action{
+		background: #FFFFFF;
+		box-shadow: 0px 9px 17px 0px rgba(98,157,252,0.15);
+		border-radius: 8.88px;
+		display: flex;
+		margin: 10rpx 20rpx;
+		padding: 22rpx 10rpx;
+		.control_action_item{
+			width: 25%;
+			text-align: center;
+			.control_action_img{
+				width: 75rpx;
+				height: 75rpx;
+			}
+			.control_action_title{
+				font-size: 26rpx;
+			}
+		}
+	}
+	.control_event{
+		margin: 20rpx;
+		padding: 40rpx 0;
+	}
 }
 </style>
---
+++ src/pages/login/login.vue
@@ -0,0 +1,117 @@
+<template>
+	<view class="login">
+		<div class="login_input">
+			<input class="input" type="text" placeholder="请输入账号" v-model="account"/>
+		</div>
+		<div class="login_input">
+			<input class="input" type="password" placeholder="请输入账号" v-model="password"/>
+		</div>
+		<div class="login_checkbox">
+			<checkbox-group @change="checkboxChange">
+				<label>
+					<checkbox value="name" style="transform:scale(0.8)" />本人已阅读并同意 <span @click.stop="privacy" class="cor">隐私政策</span> 和 <span @click.stop="privacy" class="cor">用户协议</span>
+				</label>
+			</checkbox-group>
+		</div>
+		<div class="login_checkbox">
+			<button type="primary" @click="login(0)">登录</button>
+		</div>
+		<div class="login_checkbox">
+			<button type="default" open-type="getUserInfo" @getuserinfo="wxLogin" @click="login(1)">微信一键登录</button>
+		</div>
+	</view>
+</template>
+
+<script lang="ts">
+import { defineComponent ,ref} from 'vue'
+
+export default defineComponent({
+    setup() {
+        const account = ref("")
+        const password = ref("")
+        const isChecked = ref(false)
+		// 隐私政策
+		const privacy = ()=>{
+			console.log('隐私政策')
+		}
+		// 勾选协议
+		const checkboxChange = (e: any)=>{
+			console.log(e,36)
+			isChecked.value = e.detail.value.length === 0 ? false : true;
+			console.log(isChecked.value ,40)
+		}
+		
+		// 账号登录
+		const login = (id:number)=>{
+			if(isChecked.value === false){
+				uni.showToast({
+					title: `请勾选协议`,
+					icon:'none'
+				})
+				return
+			}
+			
+			if(account.value === ''){
+				uni.showToast({
+					title: `请输入账号`,
+					icon:'none'
+				})
+				return
+			}
+
+			if(password.value === ''){
+				uni.showToast({
+					title: `请输入密码`,
+					icon:'none'
+				})
+				return
+			}
+		}
+		
+		// 微信一键登录
+		const wxLogin = ()=>{
+			uni.login({
+				provider:"weixin",
+				success:(res)=>{
+					console.log(res);
+					// 获取微信登录的授权码
+					var code = res.code
+					//在写发送request请求，
+					uni.request({})
+				}
+			})
+		}
+
+		return {
+			account,
+			password,
+			isChecked,
+			privacy,
+			login,
+			checkboxChange,
+			wxLogin
+		}
+    },
+})
+</script>
+
+<style lang="scss" scoped>
+.login{
+	.login_input{
+		margin:40rpx 40rpx;
+		border-bottom: 1.1px solid rgba(226,226,226,1);
+		padding-bottom: 20rpx;
+		.input{
+			line-height: 30rpx;
+			color:  #D0D0D0;
+		}
+	}
+	.login_checkbox{
+		margin:40rpx 40rpx;
+		padding-bottom: 20rpx;
+		.cor{
+			color: #3399FF;
+		}
+	}
+}
+</style>
