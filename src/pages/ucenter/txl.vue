3a1605ae74a2ee8a2d51a2fce3b7b09f56495ad0
yunfeizhang@chutianyun.gov.cn <zyfjz9593>
2022-08-16 17:17:34 +0800

    格式化数据

--- src/pages/ucenter/txl.vue
+++ src/pages/ucenter/txl.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-	import { defineComponent, ref } from 'vue'
+	import { defineComponent, ref, onMounted } from 'vue'
 	import vPinyin from './txl/vue-py'
 	//引入的演示数据，替换自己的后可以删除组件目录下 shujuliebiao.js文件
 	import obj from "@/uni_modules/xiaochao-indexed-list/components/xiaochao-indexed-list/shujuliebiao.js";
@@ -21,6 +21,44 @@
 					title: e.linshi,
 				});
 			}
+			// 格式化数据
+			const getCountryList = (resData)=>{
+				let res = resData;
+				this.shortcut=[];
+				// 格式化数据
+				var map = {};
+				res.forEach((item, index) => {
+				var key = item.orgCode.slice(0, 1);
+				if (!map[key]) {
+					this.shortcut.push(key)
+					map[key] = {
+					title: key,
+					items: []
+					}
+				}
+				map[key].items.push(item);
+				})
+				// 转为数组
+				var ret = [];
+				for (var k in map) {
+				var val = map[k];
+				ret.push(val);
+				}
+				// 排序
+				ret.sort((a, b)=>{
+				return a.title.charCodeAt(0) - b.title.charCodeAt(0);
+				});
+				this.shortcut.sort((a, b) =>{
+				return a.charCodeAt(0) - b.charCodeAt(0);
+				});
+				shuju.value = ret;
+			// })
+			}
+
+			onMounted(()=>{
+				let list = []
+				// getCountryList(list)
+			})
 			return {
 				shuju,
 				bindClick
